99b0d774059bdd9b41580a8ebddbbc3b63d1e94c
riuzaki16 <riuzakii16@gmail.com>
2025-07-26 23:52:19 +0300

    context with cancel

--- cmd/main.go
+++ cmd/main.go
@@ -10,19 +10,29 @@ import (
 	"go/adv-demo/pkg/db"
 	"go/adv-demo/pkg/middleware"
 	"net/http"
+	"time"
 )
 
-func main() {
-	type key int
-	const EmailKey keygi = 0
-	ctx := context.Background()
-	ctxWithValue := context.WithValue(ctx, EmailKey, "a@a.ru")
-
-	if userEmail, ok := ctxWithValue.Value(EmailKey).(string); ok {
-		fmt.Println(userEmail)
-	} else {
-		fmt.Println("No value")
+func tickOperation(ctx context.Context) {
+	ticker := time.NewTicker(200 * time.Millisecond)
+	for {
+		select {
+		case <- ticker.C:
+			fmt.Println("tick")
+		case <- ctx.Done():
+			fmt.Println("cancel")
+			return
+		}
 	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go tickOperation(ctx)
+
+	time.Sleep(2 * time.Second)
+	cancel()
+	time.Sleep(2 * time.Second) 
 }
 
 func main2() {
