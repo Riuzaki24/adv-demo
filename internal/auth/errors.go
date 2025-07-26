0e71af90c2817e9a1d6ed1fc4ec7d784a5360534
riuzaki16 <riuzakii16@gmail.com>
2025-07-26 14:29:29 +0300

    context with timeout

--- internal/auth/errors.go
+++ internal/auth/errors.go
@@ -2,4 +2,5 @@ package auth
 
 const (
 	ErrUserExisted = "user exists"
+	ErrWrongCredentials = "wrong user email or password"
 )
--- internal/auth/service.go
+++ internal/auth/service.go
@@ -3,9 +3,9 @@ package auth
 import (
 	"errors"
 	"go/adv-demo/internal/user"
+
+	"golang.org/x/crypto/bcrypt"
 )
-
-
 
 type AuthService struct {
 	UserRepository *user.UserRepository
@@ -17,21 +17,37 @@ func NewAuthService(userRepository *user.UserRepository) *AuthService {
 	}
 }
 
+func (service *AuthService) Login(email, password string) (string, error) {
+	existedUser, _ := service.UserRepository.FindByEmail(email)
+	if existedUser == nil {
+		return "", errors.New(ErrWrongCredentials)
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(existedUser.Password), []byte(password))
+	if err != nil {
+		return "", errors.New(ErrWrongCredentials)
+	}
+	return existedUser.Email, nil
+}
+
 func (service *AuthService) Register(email, password, name string) (string, error) {
 	existedUser, _ := service.UserRepository.FindByEmail(email)
 	if existedUser != nil {
 		return "", errors.New(ErrUserExisted)
 	}
 
-	user := &user.User {
-		Email: email,
-		Password: "",
-		Name: name,
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", nil
+	}
+	user := &user.User{
+		Email:    email,
+		Password: string(hashedPassword),
+		Name:     name,
 	}
 
-	_, err := service.UserRepository.Create(user)
+	_, err = service.UserRepository.Create(user)
 	if err != nil {
 		return "", err
-	}	
+	}
 	return user.Email, nil
 }
